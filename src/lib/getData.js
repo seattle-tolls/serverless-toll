c076c68f14c118a8006eae1052ea46ae67c37568
ronbarrantes <ronbarrantes@gmail.com>
2019-11-23 17:50:08 -0800

    Fetching the full html from the pages

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,9 +1,21 @@
-const getData = (url) => {
+const cheerio = require('cheerio')
+const fetch = require('node-fetch')
+
+const getData = async (url) => {
+
+  const link = await fetch(url)
+  const buff = await link.buffer()
+  const html = await buff.toString()
+
+  console.log( 'HTML -->', html)
+
+    const data =  {
+      weekdays: html,
+      weekend: html
+  }
 
   return (
-    {
-      theDataFrom: url
-    }
+  data
   )
 }
 
